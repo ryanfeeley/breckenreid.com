778e5705f3c9879977ab2a9c04232625c7e2f662
Ryan Feeley <ryan@macbookair.lan>
2026-02-19 23:45:47 -0500

    Fix heading color vars and GitHub Pages asset serving

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+const site = process.env.SITE_URL || 'https://clay-astro-theme.netlify.app';
+const base = process.env.BASE_PATH || '/';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://clay-astro-theme.netlify.app',
+    site,
+    base,
     integrations: [sitemap()],
 });
